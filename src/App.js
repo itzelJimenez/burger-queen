0b71e4bd4c7ce68040302b7a6368082e84c2a83a
itzeljimenez <itzeljimenez.do@gmail.com>
2019-12-12 11:56:16 -0600

    remove unussed code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Layout } from './routing/Layout/Layout';
-
-//import Home from './pages/Home';
-//import Admin from './pages/Admin';
+import { LoginForm } from './components/Login/Login';
 
 function App() {
-  /*<Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home Page</Link>
-        </li>
-        <li>
-          <Link to="/admin">Admin Page</Link>
-        </li>
-      </ul>
-      <Route exact path="/" component={Home} />
-      <Route path="/admin" component={Admin} />
-    </div>
-    </Router>*/
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Layout} />
+        <Layout component={LoginForm} />
       </Switch>
     </Router>
   );
