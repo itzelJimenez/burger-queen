51a29a5b3a424c3fda081207c5253ebbaef0a9c8
itzeljimenez <itzeljimenez.do@gmail.com>
2019-12-12 21:09:36 -0600

    updat dashboard component

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import clsx from 'clsx';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ListItems from '../ListItems/ListItems';
+import Order from '../Order/Order';
+import { ToolbarComponent } from '../ToolbarComponent/ToolbarComponent';
+import { useStyles } from '../Styles/Styles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Burguer Queen
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function Dashboard({ component: Component, title }) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <ToolbarComponent title={title} />
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose)
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <ListItems />
+        <Divider />
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            {/*CONTENT */}
+            {/* Recent Deposits */}
+            <Grid item xs={12} md={8} lg={9}>
+              <Component />
+            </Grid>
+            {/* Recent Orders */}
+            <Grid item xs={12} md={4} lg={3}>
+              <Order />
+            </Grid>
+          </Grid>
+          <Box pt={4}>
+            <Copyright />
+          </Box>
+        </Container>
+      </main>
+    </div>
+  );
+}
