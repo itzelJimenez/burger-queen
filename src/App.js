d3fd9187001dcd77bf060c7940c594887437eb2c
itzeljimenez <itzeljimenez.do@gmail.com>
2019-12-12 20:59:44 -0600

    add routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Layout } from './routing/Layout/Layout';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { LoginForm } from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+import { BreakfastMenu } from './components/BreakefastMenu/BreakfastMenu';
+import { Home } from './routing/Home/Home';
+import { Menu } from './components/Menu/Menu';
 
 function App() {
+  let breakfastComponent = () => {
+    return <Dashboard component={BreakfastMenu} title="Desayuno" />;
+  };
+
+  let menuComponent = () => {
+    return <Dashboard component={Menu} title="Menú" />;
+  };
+
   return (
     <Router>
       <Switch>
-        <Layout component={LoginForm} />
+        <Route path="/breakfast" component={breakfastComponent} />
+        <Route path="/menu" component={menuComponent} />
+        <Route path="/dashboard" component={breakfastComponent} />
+        <Home path="/" component={LoginForm} />
       </Switch>
     </Router>
   );
